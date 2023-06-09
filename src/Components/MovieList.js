e7ac3ee2da4f71a88975e274fa2b88597c546bcb
The Last One <thelastonent@gmail.com>
2023-06-08 21:25:54 -0400

    Added carousel

--- src/Components/MovieList.js
+++ src/Components/MovieList.js
@@ -4,6 +4,15 @@ import img3 from "../Movie images/drakon2.webp";
 import img4 from "../Movie images/drakon3.webp";
 import img5 from "../Movie images/nemo.jpg";
 import img6 from "../Movie images/dori.jpg";
+import img7 from "../Movie images/7.webp";
+import img8 from "../Movie images/8.jpg";
+import img9 from "../Movie images/9.jpg";
+import img10 from "../Movie images/10.jpg";
+import img11 from "../Movie images/11.jpg";
+import img12 from "../Movie images/12.jpg";
+import img13 from "../Movie images/13.jpg";
+import img14 from "../Movie images/14.jpg";
+import img15 from "../Movie images/15.jpg";
 
 const movieList = [
   {
@@ -41,6 +50,60 @@ const movieList = [
     name: "В поисках Дори",
     genre: "Crime",
     image: img6,
+  },
+  {
+    id: 7,
+    name: "Законопослушный гражданин",
+    genre: "Crime",
+    image: img7,
+  },
+  {
+    id: 8,
+    name: "Дом",
+    genre: "Crime",
+    image: img8,
+  },
+  {
+    id: 9,
+    name: "Голодные игры",
+    genre: "Crime",
+    image: img9,
+  },
+  {
+    id: 10,
+    name: "Мандарины",
+    genre: "Crime",
+    image: img10,
+  },
+  {
+    id: 11,
+    name: "Отец солдата",
+    genre: "Crime",
+    image: img11,
+  },
+  {
+    id: 12,
+    name: "Фокус",
+    genre: "Crime",
+    image: img12,
+  },
+  {
+    id: 13,
+    name: "Однажды в Америке",
+    genre: "Crime",
+    image: img13,
+  },
+  {
+    id: 14,
+    name: "Брюс Всемогущий",
+    genre: "Crime",
+    image: img14,
+  },
+  {
+    id: 15,
+    name: "Зима в огне",
+    genre: "Crime",
+    image: img15,
   },
 ];
 
